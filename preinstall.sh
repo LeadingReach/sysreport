06c5154f2d21eceda0ad87ae44a0b520f3e6ff4b
carlpetry <102311970+carlpetry@users.noreply.github.com>
2023-01-24 11:37:52 -0600

    Delete preinstall.sh

--- preinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ -f /Library/LaunchDaemons/com.leadingreach.sysreport.plist ]]; then
-    launchctl unload /Library/LaunchDaemons/com.leadingreach.sysreport.plist
-    rm /Library/LaunchDaemons/com.leadingreach.sysreport.plist
-fi
